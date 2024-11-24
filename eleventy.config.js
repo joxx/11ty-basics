90de7096ff05904d394c84b52ceb928eb1f54f72
Joachim Kliemann <joachim.kliemann@googlemail.com>
2024-11-24 22:29:39 +0100

    update header

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,12 +1,15 @@
 export default function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/assets/");
   eleventyConfig.addWatchTarget("src/assets/**");
-}
 
-export const config = {
-  dir: {
-    input: "src",
-    output: "_site",
-    layouts: "_layouts",
-  },
-};
+  return {
+    dir: {
+      input: "src",
+      output: "_site",
+      includes: "_includes",
+      layouts: "_layouts",
+      data: "data",
+    },
+    templateFormats: ["njk", "md", "html"],
+  };
+}
